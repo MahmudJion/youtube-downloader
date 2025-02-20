98a121f6025ce4684045e17b30a60e918a1294f4
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2025-02-20 07:33:42 +0000

    add user profile retrieval and user creation routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET user profile. */
+router.get('/:id', function(req, res, next) {
+  const userId = req.params.id;
+  res.send(`User profile for user with ID: ${userId}`);
+});
+
+/* POST create new user. */
+router.post('/', function(req, res, next) {
+  const newUser = req.body;
+  // Add logic to create a new user
+  res.status(201).send(`User created with name: ${newUser.name}`);
+});
+
 module.exports = router;
